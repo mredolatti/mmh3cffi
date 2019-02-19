b27bb8702f862c7544db4fd3a7cd5c3b7a5fce19
Martin Redolatti <martinredolatti@gmail.com>
2019-02-19 13:49:29 -0300

    bump version and add changelog

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ classifiers = [
 
 setup(
     name='mmh3cffi',
-    version='0.1.1',
+    version='0.1.2',
     description='CFFI interface to murmurhash3/32 from aappleby/smhasher',
     license='Apache 2.0',
     author='Split software',
