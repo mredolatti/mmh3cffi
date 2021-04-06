5394feecfd7be25a511a1e3f8253fae45c7d6d07
Martin Redolatti <martin.redolatti@split.io>
2021-04-06 16:23:25 -0300

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ classifiers = [
 
 setup(
     name='mmh3cffi',
-    version='0.2.0',
+    version='0.2.1',
     description='CFFI interface to murmurhash3/32 from aappleby/smhasher',
     license='Apache 2.0',
     author='Split software',
