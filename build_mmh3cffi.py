eb665160e4e1e3116e8d69e70ebce9e0c6234fa7
Martin Redolatti <martin.redolatti@split.io>
2021-04-06 16:23:06 -0300

    require C99 standard to build the C extension

--- build_mmh3cffi.py
+++ build_mmh3cffi.py
@@ -11,7 +11,8 @@ FFI_BUILDER.set_source(
     'mmh3cffi._cimpl',
     '#include "mmh3.h"',
     sources=[os.path.join('csrc', 'mmh3.c')],
-    include_dirs=['csrc']
+    include_dirs=['csrc'],
+    extra_compile_args=['-std=c99']
 )
 
 if __name__ == '__main__':
